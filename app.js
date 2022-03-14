3ddbdb8a1b60ee80c7ed2f7ae8689f807a32ab18
choidonghun <choidonghun0831@gmail.com>
2022-03-14 16:06:02 +0900

    week02

---
+++ app.js
@@ -0,0 +1,10 @@
+const js = document.getElementById("JS");
+const html = document.getElementById("htmml");
+js.addEventListener("click", J);
+html.addEventListener("click", K);
+function K() {
+  alert("응 아니야 돌아가 겁나 어려워 응 아니야");
+}
+function J() {
+  alert("Coach ran away");
+}

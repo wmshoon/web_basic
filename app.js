a93c365de8b7eb5526329d73f6760abf4a1571ae
choidonghun <choidonghun0831@gmail.com>
2022-04-03 20:02:12 +0900

    idk

--- app.js
+++ app.js
@@ -11,28 +11,28 @@ j.addEventListener("click", jj);
 function K() {
   alert("응 아니야 돌아가 겁나 어려워 응 아니야");
 }
+
 function J() {
-  const hehe = Math.random();
-  if (hehe > 0.5) {
+  const rr = Math.random();
+  if (rr > 0.5) {
     alert("Coach ran away");
-  } else if (hehe < 0.5) {
+  } else if (rr < 0.5) {
     alert("404 coach not found");
   }
 }
 function ronny() {
-  const hehe = Math.random();
-  if (hehe > 0.5) {
+  const rr = Math.random();
+  if (rr > 0.5) {
     alert("i am ron");
-  } else if (hehe < 0.5) {
+  } else if (rr < 0.5) {
     alert("i am dumb");
   }
 }
 function jj() {
-  const hehe = Math.random();
-  if (hehe > 0.5) {
+  const rr = Math.random();
+  if (rr > 0.5) {
     alert("i am j");
-  } else if (hehe < 0.5) {
+  } else if (rr < 0.5) {
     alert("i am dumb");
   }
 }
-function randoM() {}

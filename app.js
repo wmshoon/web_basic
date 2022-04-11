90c87a9fe763c3e72e37bbd22771561f99de4283
choidonghun <choidonghun0831@gmail.com>
2022-04-11 14:03:55 +0900

    box 모델 써먹기

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
-const js = document.getElementById("JS");
-const html = document.getElementById("htmml");
-const ron = document.getElementById("ron");
-const j = document.getElementById("J");
+const coach = Document.getelementById("coach");
+coach.addEventListener("click", Coach);
 
-js.addEventListener("click", J);
-html.addEventListener("click", K);
-ron.addEventListener("click", ronny);
-j.addEventListener("click", jj);
-
-function K() {
-  alert("응 아니야 돌아가 겁나 어려워 응 아니야");
-}
-
-function J() {
-  const rr = Math.random();
-  if (rr > 0.5) {
+function Coach() {
+  const a = Math.random();
+  if (a > 0.5) {
     alert("Coach ran away");
-  } else if (rr < 0.5) {
+  } else if (a < 0.5) {
     alert("404 coach not found");
-  }
-}
-function ronny() {
-  const rr = Math.random();
-  if (rr > 0.5) {
-    alert("i am ron");
-  } else if (rr < 0.5) {
-    alert("i am dumb");
-  }
-}
-function jj() {
-  const rr = Math.random();
-  if (rr > 0.5) {
-    alert("i am j");
-  } else if (rr < 0.5) {
-    alert("i am dumb");
   }
 }
